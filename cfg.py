f980c6be600f219205abffdbf9904a37b7569ddd
G-man <61664874+vovamod@users.noreply.github.com>
2023-01-06 18:00:52 +0200

    Update config file
    Added explanations for variables that needed to be changed if necessary

--- cfg.py
+++ cfg.py
@@ -1,4 +1,14 @@
+# Config for DDS converter
+# Ver: 2.1a2
+
+# Directory where the files that needed to be converted are stored. Default: "DDS"
 dir_1 = "DDS"
+
+# Directory where files that was converted will be stored. Default: "PNG"
 dir_2 = "PNG"
+
+# File format of files that needed to be converted
 ft_1 = ".dds"
+
+# File format of files that will be converted
 ft_2 = ".png"
